0f8f9f5d2af0eacaf6198e6599ab194d3a197953
ccisgood <chenchengynx@gmail.com>
2017-11-08 20:06:18 -0500

    199: Binary Tree Right Side View. 2 solutions

---
+++ medium/199. Binary Tree Right Side View.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    var res = [];
+    traverse(root, res, 0);
+    return res;
+};
+
+function traverse(node, res, level) {
+    if (node == null) {
+        return;
+    }
+    if (res.length <= level) {
+        res.push(node.val);
+    }
+    traverse(node.right, res, level + 1);
+    traverse(node.left, res, level + 1);
+}
+
+//try to do it in iteration way
+var rightSideView2 = function(root) {
+    var res = [];
+    if (!root) {
+        return res;
+    }
+    var queue = [];
+    queue.push(root);
+    while (queue.length) {
+        var nextLevel = [];
+        res.push(queue[0].val);
+        while (queue.length) {
+            var node = queue.shift();
+            if (node.right != null) {
+                nextLevel.push(node.right);
+            }
+            if (node.left != null) {
+                nextLevel.push(node.left);
+            }
+        }
+        queue = nextLevel;
+    }
+    return res;
+}
